cf6913830c2897e2eb6a4e6b7f22341ce1a8206c
Jeremy Grifski <jeremy.grifski@gmail.com>
2020-11-15 00:44:34 -0500

    Wrote successful month query

--- Script2/python_solution/tutorial/graph_helper.py
+++ Script2/python_solution/tutorial/graph_helper.py
@@ -3,6 +3,7 @@
 
 # <FirstCodeSnippet>
 from requests_oauthlib import OAuth2Session
+import datetime
 
 graph_url = 'https://graph.microsoft.com/v1.0'
 
@@ -21,11 +22,15 @@ def get_user(token):
 def get_calendar_events(token):
     graph_client = OAuth2Session(token=token)
 
+    first_day = datetime.datetime.today().replace(day=1)
+    next_first_day = (first_day + datetime.timedelta(days=32)).replace(day=1)
+
     # Configure query parameters to
     # modify the results
     query_params = {
         '$select': 'subject,organizer,start,end',
-        '$orderby': 'start/dateTime ASC'
+        '$orderby': 'start/dateTime ASC',
+        '$filter': f'start/dateTime ge \'{first_day}\' and start/dateTime lt \'{next_first_day}\''
     }
 
     # Send GET to /me/events
