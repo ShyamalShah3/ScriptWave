5f2f96005a289b155fd28d2f8a2564eb1d688faf
Jeremy Grifski <jeremy.grifski@gmail.com>
2020-11-15 00:19:50 -0500

    Remove binaries

--- Script2/python_solution/tutorial/graph_helper.py
+++ Script2/python_solution/tutorial/graph_helper.py
@@ -6,27 +6,31 @@ from requests_oauthlib import OAuth2Session
 
 graph_url = 'https://graph.microsoft.com/v1.0'
 
+
 def get_user(token):
-  graph_client = OAuth2Session(token=token)
-  # Send GET to /me
-  user = graph_client.get('{0}/me'.format(graph_url))
-  # Return the JSON result
-  return user.json()
+    graph_client = OAuth2Session(token=token)
+    # Send GET to /me
+    user = graph_client.get('{0}/me'.format(graph_url))
+    # Return the JSON result
+    return user.json()
 # </FirstCodeSnippet>
 
 # <GetCalendarSnippet>
+
+
 def get_calendar_events(token):
-  graph_client = OAuth2Session(token=token)
+    graph_client = OAuth2Session(token=token)
 
-  # Configure query parameters to
-  # modify the results
-  query_params = {
-    '$select': 'subject,organizer,start,end',
-    '$orderby': 'createdDateTime DESC'
-  }
+    # Configure query parameters to
+    # modify the results
+    query_params = {
+        '$select': 'subject,organizer,start,end',
+        '$orderby': 'start/dateTime ASC'
+    }
 
-  # Send GET to /me/events
-  events = graph_client.get('{0}/me/events'.format(graph_url), params=query_params)
-  # Return the JSON result
-  return events.json()
+    # Send GET to /me/events
+    events = graph_client.get(
+        '{0}/me/events'.format(graph_url), params=query_params)
+    # Return the JSON result
+    return events.json()
 # </GetCalendarSnippet>
