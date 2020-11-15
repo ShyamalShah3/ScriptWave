211938b2b506a0d71aa25ec9b1378dc98f96f9b9
Shyamal <shyamalgs@gmail.com>
2020-11-15 11:13:33 -0500

    Weekly View (Shyamal)

--- Script2/graph-tutorial/src/app/weekly/weekly.component.ts
+++ Script2/graph-tutorial/src/app/weekly/weekly.component.ts
@@ -42,17 +42,70 @@ export class WeeklyComponent implements OnInit {
   }
   // </formatDateTimeTimeZoneSnippet>
 
-  makeDateArray(dateTime: DateTimeTimeZone): any{
+  getDate(dateTime: DateTimeTimeZone): any{
+    try {
+      return (dateTime.dateTime.substr(8, 2));
+    }
+    catch (error) {
+      this.alertsService.add('DateTimeTimeZone conversion error', JSON.stringify(error));
+    }
+  }
+
+  formatDate(dateTime: DateTimeTimeZone): string {
     try {
       var year = dateTime.dateTime.substr(0, 4);
       var month = dateTime.dateTime.substr(5, 2);
       var date = dateTime.dateTime.substr(8, 2);
+      var monthString;
 
+      switch (month) {
+        case "01":
+          monthString = 'January';
+          break;
+        case "02":
+          monthString = 'February';
+          break;
+        case "03":
+          monthString = 'March';
+          break;
+        case "04":
+          monthString = 'April';
+          break;
+        case "05":
+          monthString = 'May';
+          break;
+        case "06":
+          monthString = 'June';
+          break;
+        case "07":
+          monthString = 'July';
+          break;
+        case "08":
+          monthString = 'August';
+          break;
+        case "09":
+          monthString = 'September';
+          break;
+        case "10":
+          monthString = 'October';
+          break;
+        case "11":
+          monthString = 'November';
+          break;
+        case "12":
+          monthString = 'December';
+          break;
+        default:
+          throw "FORMAT DATE ERROR";
+      }
+
+      return (monthString + ', ' + date + ', ' + year);
 
     }
     catch (error) {
       this.alertsService.add('DateTimeTimeZone conversion error', JSON.stringify(error));
     }
   }
+
 }
 
